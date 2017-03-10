1a1d62fd8e933e76ee51ec9f86f7f528b0258964
Vitalii Ganzha <vganzha@tableau.com>
2017-03-10 12:52:22 -0800

    added selenium packages

--- BingSearchSim/Program.cs
+++ BingSearchSim/Program.cs
@@ -1,10 +1,21 @@
 ﻿
 namespace BingSearchSim
 {
+    using OpenQA.Selenium;
+    using OpenQA.Selenium.Chrome;
+    using OpenQA.Selenium.Firefox;
+
     class Program
     {
         static void Main(string[] args)
         {
+            IWebDriver driver = new ChromeDriver();
+
+            driver.Navigate().GoToUrl("https://www.bing.com/");
+            IWebElement query = driver.FindElement(By.Name("q"));
+            query.SendKeys("Cheese");
+            System.Console.WriteLine("Page title is: " + driver.Title);
+            driver.Quit();
         }
     }
 }
